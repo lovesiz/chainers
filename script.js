cba6cac590f33f9bacccb0da3cdd663d0fe22d37
lovesiz <115664111+lovesiz@users.noreply.github.com>
2025-12-12 01:59:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function hesapla() {
   }
 
   // Tablo 1 katsayıları
-  const katsayilar1 = [0.0015, 95, 5];
+  const katsayilar1 = [0.0015, 120, 8];
   const ids1 = ['t1s1', 't1s2', 't1s3'];
 
   // Tablo 2 katsayıları
-  const katsayilar2 = [0.017, 815, 60];
+  const katsayilar2 = [0.017, 1100, 100];
   const ids2 = ['t2s1', 't2s2', 't2s3'];
 
   // Tablo 3 katsayıları
-  const katsayilar3 = [0.0315, 1595, 117];
+  const katsayilar3 = [0.0315, 1800, 160];
   const ids3 = ['t3s1', 't3s2', 't3s3'];
 
   // Tüm tabloları hesapla
@@ -27,4 +27,5 @@ function hesapla() {
     });
   });
 }
+
 
